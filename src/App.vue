e13496bb50bce0942629bcac6f5ea23a2d962a8e
faisalhatila <hatilas1994@gmail.com>
2021-02-04 19:28:15 +0500

    project done

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!-- <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.another }}</p>
+    </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader";
+// import BadgeList from "./components/BadgeList";
+// import UserInfo from "./components/UserInfo";
+// import CourseGoals from "./components/CourseGoals";
+import ActiveGoals from "./components/ActiveGoals";
+import ManageGoals from "./components/ManageGoals";
+
+export default {
+  components: {
+    "the-header": TheHeader,
+    // "badge-list": BadgeList,
+    // "user-info": UserInfo,
+    // "course-goals": CourseGoals,
+    "active-goals": ActiveGoals,
+    "manage-goals": ManageGoals,
+  },
+  data() {
+    return {
+      selectedComponent: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <error-alert v-if="inputIsValid">
+      <h2>Input is invald</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmGoal">Okay</button>
+    </error-alert>
+  </teleport>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsValid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsValid = true;
+        return;
+      } else {
+        this.inputIsValid = false;
+        return;
+      }
+    },
+    confirmGoal() {
+      this.inputIsValid = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
